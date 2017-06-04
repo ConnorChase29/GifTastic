c23070656cbc06d1adcb933f0cf88313d3a94957
Connor Chase <connorchase@Connors-MacBook-Pro.local>
2017-06-04 16:03:39 -0400

    So close.

--- javascript/web.js
+++ javascript/web.js
@@ -2,14 +2,19 @@ $( document ).ready(function() {
 
   var animalsArray = ["dog", "cat", "rabbit", "hampster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
 
-  for (var i = 0; i < animalsArray.length; i++) {
-    var animalsButton = $("<button>");
-    animalsButton.addClass("gif");
-    animalsButton.attr("data-name", animalsArray[i]);
-    animalsButton.text(animalsArray[i]);
-    $("#animals-view").append(animalsButton);
-  }
+  function renderButtons() {
 
+    //$("#animals-view").empty();
+
+    for (var i = 0; i < animalsArray.length; i++) {
+      var animalsButton = $("<button>");
+      animalsButton.addClass("gif");
+      animalsButton.attr("data-name", animalsArray[i]);
+      animalsButton.text(animalsArray[i]);
+      $("#animals-view").append(animalsButton);
+    }
+  }
+  
   $("button").on("click", function() {
     var animal = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
@@ -35,6 +40,20 @@ $( document ).ready(function() {
       }
     });
   });
+
+  $("#add-animal").on("click", function() {
+
+    event.preventDefault();
+
+    var animalUserInput = $("#animal-input").val().trim();
+    animalsArray.push(animalUserInput);
+    renderButtons();
+    /* var newAnimalButton = $("<button>");
+    newAnimalButton.addClass("gif");
+    newAnimalButton.attr("data-name", animalUserInput);
+    newAnimalButton.text(animalUserInput);
+    $("#animals-view").append(newAnimalButton); */
+  })
+  renderButtons();
+
 });
-
-
